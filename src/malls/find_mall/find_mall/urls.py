d2218202f7d0b88861be880589dc155b755fa3bf
ItSeals <itseals.pm@gmail.com>
2023-02-02 22:02:28 +0200

    image show

--- src/malls/find_mall/find_mall/urls.py
+++ src/malls/find_mall/find_mall/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('api/v1/', include(findmall_urls))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
