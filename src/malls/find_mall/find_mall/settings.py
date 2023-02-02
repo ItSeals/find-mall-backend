2aceb2155617c94f3cc5ff5ec9ca5c88990b8f1d
ItSeals <itseals.pm@gmail.com>
2023-02-03 01:53:54 +0200

    image show 7

--- src/malls/find_mall/find_mall/settings.py
+++ src/malls/find_mall/find_mall/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/media/'
-MEDIA_ROOT = ''
+STATIC_URL = '/find_mall/'
+MEDIA_ROOT = '/media/'
 MEDIA_URL = ''
 
 # Default primary key field type
